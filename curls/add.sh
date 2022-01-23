69db67371457caa55f18761e117e67f70a5c9479
Bruno Bossola <bruno@meterian.com>
2022-01-23 23:01:02 +0000

    Changes for Jan22 demo

--- curls/add.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-set -x
-curl -i -X POST -d '{"name":"melon","description":"Real melon from Italy", "data":["java.util.HashMap",{"cost":2,"color":"yellow"}]}' http://localhost:8888/products; echo
--- curls/list.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-set -x
-curl -i http://localhost:8888/products; echo
